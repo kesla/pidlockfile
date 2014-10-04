762d18b05a99abdab5967ab6680af47e6e3f9120
David Björklund <david.bjorklund@gmail.com>
2014-10-04 18:14:15 +0200

    DRY

--- pidlockfile.js
+++ pidlockfile.js
@@ -2,34 +2,14 @@ var fs = require('fs')
 
   , pid = process.pid.toString()
 
-  , checkPid = function (filename, callback) {
+  , check = function (filename, callback) {
       fs.readFile(filename, { encoding: 'utf8' }, function (err, otherPid) {
-        if (err && err.code === 'ENOENT')
-          return callback()
-        else if (err)
-          return callback(err)
+        if (err) return callback(err)
 
         otherPid = parseInt(otherPid, 10)
 
         try {
           process.kill(otherPid, 0)
-          callback(new Error('Lockfile already acquired'))
-        } catch(e) {
-          // remove the pidfile if the previous process didn't remove it
-          // properly
-          fs.unlink(filename, callback)
-        }
-      })
-    }
-
-  , check = function (filename, callback) {
-      fs.readFile(filename, { encoding: 'utf8' }, function (err, pid) {
-        if (err) return callback(err)
-
-        pid = parseInt(pid, 10)
-
-        try {
-          process.kill(pid, 0)
           callback(null, true)
         } catch(e) {
           callback(null, false)
@@ -38,12 +18,21 @@ var fs = require('fs')
     }
 
   , lock = function (filename, callback) {
-
-      checkPid(filename, function(err) {
-        if (err)
-          return callback(err)
-
-        fs.writeFile(filename, pid, { flag: 'wx' }, callback)
+      check(filename, function (err, locked) {
+        if (err && err.code === 'ENOENT') {
+          fs.writeFile(filename, pid, { flag: 'wx' }, callback)
+        } else if (err || locked && (err = new Error('Lockfile already acquired'))) {
+          callback(err)
+        } else {
+          //not error and file isn't locked, so must unlink file first
+          fs.unlink(filename, function (err) {
+            if (err) {
+              callback(err)
+            } else {
+              fs.writeFile(filename, pid, { flag: 'wx' }, callback)
+            }
+          })
+        }
       })
     }
   , unlock = function (filename, callback) {
